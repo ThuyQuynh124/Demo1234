f48a89f2b214bde69b9ded1fc5cd14987b4508b5
LE THUY QUYNH <1724010123@student.humg.edu.vn>
2021-12-16 17:08:02 +0700

    Create_Table_Person_Demo1234

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Demo1234.Models;
+
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext (DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Demo1234.Models.Person> Person { get; set; }
+    }
---
+++ Migrations/20211216100655_Create_Table_Person_Demo1234.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Demo1234.Migrations
+{
+    public partial class Create_Table_Person_Demo1234 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PersonDemo1234",
+                columns: table => new
+                {
+                    PersonID = table.Column<string>(type: "TEXT", nullable: false),
+                    PersonName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonDemo1234", x => x.PersonID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonDemo1234");
+        }
+    }
+}
